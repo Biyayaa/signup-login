b3e31257d818581ec411351c82ddeb8e9723321e
Biyayaa <onaleye.ap@gmail.com>
2023-04-13 05:42:41 +0100

    added better documentation to the javascript code

--- script.js
+++ script.js
@@ -1,117 +1,131 @@
+// Hide the login page on load of the page
 document.getElementById("login-box").style.display = "none";
-let loading = document.getElementById("loading-div");
-let loadingL = document.getElementById("loading-divL");
-let fullName = document.getElementById("f-name");
-let email = document.getElementById("user-email");
+
+// Get the necessary elements from the HTML document
+let loginBox = document.getElementById("login-box");
+let signUpBox = document.getElementById("sign-up-box");
+let loadingDiv = document.getElementById("loading-div");
+let loadingDivL = document.getElementById("loading-divL");
+let fullNameInput = document.getElementById("f-name");
+let emailInput = document.getElementById("user-email");
+let passwordInput = document.getElementById("user-password");
+let confirmPasswordInput = document.getElementById("confirm-password");
 let signUpMessage = document.getElementById("signup-msg");
-let password = document.getElementById("user-password");
-let logInMessage = document.getElementById("login-msg");
+let loginEmailInput = document.getElementById("loginEm");
+let loginPasswordInput = document.getElementById("loginPass");
+let loginMessage = document.getElementById("login-msg");
 
-let confirmPassword = document.getElementById("confirm-password");
+// Hide the loading divs by default
+loadingDiv.style.display = "none";
+loadingDivL.style.display = "none";
 
-loading.style.display = "none";
-loadingL.style.display = "none";
-
+// Show the login box and hide the sign up box
 function loginPage() {
-  document.getElementById("sign-up-box").style.display = "none";
-  document.getElementById("login-box").style.display = "block";
+  signUpBox.style.display = "none";
+  loginBox.style.display = "block";
 }
 
+// Show the sign up box and hide the login box
 function signUpPage() {
-  document.getElementById("sign-up-box").style.display = "block";
-  document.getElementById("login-box").style.display = "none";
+  signUpBox.style.display = "block";
+  loginBox.style.display = "none";
 }
 
-let myObj;
-
+// Handle the sign up process
 function signUp() {
+  // Check if the password and confirm password fields match and the full name and email fields are not empty
   if (
-    password.value === confirmPassword.value &&
-    fullName.value != "" &&
-    email.value != ""
+    passwordInput.value === confirmPasswordInput.value &&
+    fullNameInput.value != "" &&
+    emailInput.value != ""
   ) {
-    myObj = {
-      fullname: fullName.value,
-      email: email.value,
-      password: password.value,
-      confirmPassword: confirmPassword.value,
+    // Create a user object with the input values
+    let user = {
+      fullname: fullNameInput.value,
+      email: emailInput.value,
+      password: passwordInput.value,
+      confirmPassword: confirmPasswordInput.value,
     };
 
-    // Check if there are already registered users in localStorage
+    // Get the array of registered users from localStorage or create an empty array if it doesn't exist
     let registeredUsers = JSON.parse(localStorage.getItem("members")) || [];
 
     // Add the new user object to the array of registered users
-    registeredUsers.push(myObj);
+    registeredUsers.push(user);
 
     // Save the updated array of registered users to localStorage
     localStorage.setItem("members", JSON.stringify(registeredUsers));
 
+    // Display a success message and hide the sign up box after 3 seconds
     signUpMessage.innerHTML = `
-        <p id="success-msg">Sign up successful!!!</p>
-        `;
-    loading.style.display = "block";
+      <p id="success-msg">Sign up successful!!!</p>
+    `;
+    loadingDiv.style.display = "block";
     setTimeout(() => {
-      document.getElementById("sign-up-box").style.display = "none";
-      document.getElementById("login-box").style.display = "block";
-      // document.getElementById("login-email").value = email.value;
-      loading.style.display = "none";
+      signUpBox.style.display = "none";
+      loginBox.style.display = "block";
+      loadingDiv.style.display = "none";
       signUpMessage.style.display = "none";
     }, 3000);
 
-    fullName.value = "";
-    email.value = "";
-    password.value = "";
-    confirmPassword.value = "";
+    // Clear the input fields
+    fullNameInput.value = "";
+    emailInput.value = "";
+    passwordInput.value = "";
+    confirmPasswordInput.value = "";
   } else {
+    // Display a failure message if the input values are invalid
     signUpMessage.innerHTML = `
-        <p id="failed-msg">Sign up failed!!!</p>
-        `;
-    setTimeout(() => {
-      signUpMessage.style.display = "none";
-    }, 3000);
-  }
-}
-
-
-let loginPass = document.getElementById("loginPass");
-let loginEm = document.getElementById("loginEm");
-function login() {
-  loadingL.style.display = "block";
-
-  setTimeout(() => {
-    loadingL.style.display = "none";
-    // Get the email entered on the login page
-  let loginEmail = loginEm.value;
-  console.log(loginEmail);
-  let loginPassword = loginPass.value;
-  console.log(loginPass);
-  // Get the array of registered users from localStorage
-  let registeredUsers = JSON.parse(localStorage.getItem("members")) || [];
-  console.log(registeredUsers);
-  // Find the user object with the matching email
-  let user = registeredUsers.find((members) => members.email === loginEmail && members.password === loginPassword);
-  console.log(user);
-
-
-  if (user) {
-    // Redirect to the dashboard page
-    window.location.href = "dashboard.html";
-
-    window.localStorage.setItem('currentUser', JSON.stringify(user));
-  } else {
-    logInMessage.innerHTML = `
-      <p id="failed-msg">Login failed! Please check your email and password.</p>
+      <p id="failed-msg">Sign up failed!!!</p>
     `;
     setTimeout(() => {
-      logInMessage.style.display = "none";
+      signUpMessage.style.display = "none";
     }, 3000);
   }
-}, 3000);
 }
 
-let getter = "";
-function getUserInfo() {
-  getter = JSON.parse(localStorage.getItem("member"));
-  console.log(getter);
+// Handle the login process
+function login() {
+  // Show the loading div
+  loadingDivL.style.display = "block";
+
+  setTimeout(() => {
+    // Hide the loading div after 3 seconds
+    loadingDivL.style.display = "none";
+
+    // Get the email and password entered on the login page
+    let loginEmail = loginEmailInput.value;
+    let loginPassword = loginPasswordInput.value;
+
+    // Get the array of registered users from localStorage or create an empty array if it doesn't exist
+    let registeredUsers = JSON.parse(localStorage.getItem("members")) || [];
+
+    // Find the user object with the matching email and password
+    let user = registeredUsers.find(
+      (member) =>
+        member.email === loginEmail && member.password === loginPassword
+    );
+
+    if (user) {
+      // Redirect to the dashboard page and store the current user in localStorage
+      window.location.href = "dashboard.html";
+      window.localStorage.setItem("currentUser", JSON.stringify(user));
+    } else {
+      // Display a failure message if the login credentials are invalid
+      loginMessage.innerHTML = `
+        <p id="failed-msg">Login failed! Please check your email and password.</p>
+      `;
+      setTimeout(() => {
+        loginMessage.style.display = "none";
+      }, 3000);
+    }
+  }, 3000);
 }
+
+// Get the current user information from localStorage
+function getUserInfo() {
+  let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  console.log(currentUser);
+}
+
 getUserInfo();
