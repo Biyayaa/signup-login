fafdaa99adb70868b9789d43c3ed78a922929a56
Biyayaa <onaleye.ap@gmail.com>
2023-04-13 06:03:27 +0100

    modified the code such that if there is no current user, accessing the dashboard from the url leads back to index page

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,27 @@
 // import { user } from "./script.js";
 
-// let dashboardWelcome = document.getElementById("welcome");
+let dashboardWelcome = document.getElementById("welcome");
 // dashboardWelcome.innerHTML = `<h1 class="dashboard-welcome"> Welcome, ${user.fullname}! <h1>`;
 
 
-let currentUser = JSON.parse(window.localStorage.getItem('currentUser'));
+// Get the welcome div and the logout button
+let welcomeDiv = document.getElementById("welcome");
+let logoutBtn = document.getElementById("logout-btn");
 
-let dashboardWelcome = document.getElementById('welcome');
-dashboardWelcome.innerHTML = `<h1 class="dashboard-welcome">Welcome, ${currentUser.fullname}!</h1>`;
+// Get the current user information from localStorage
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+// If there is no current user, redirect to the login page
+if (!currentUser) {
+    window.location.href = "index.html";
+  }
+
+// Display the welcome message with the current user's name
+dashboardWelcome.innerHTML = `Welcome, ${currentUser.fullname}!`;
+
+// Handle the logout process
+logoutBtn.addEventListener("click", () => {
+  // Remove the current user from localStorage and redirect to the login page
+  localStorage.removeItem("currentUser");
+  window.location.href = "index.html";
+});
