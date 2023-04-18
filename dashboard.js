c98962f79a076e86edac9a3ad3137705f638bd04
Biyayaa <onaleye.ap@gmail.com>
2023-04-18 06:57:30 +0100

    images attached also show up on view posts page

--- dashboard.js
+++ dashboard.js
@@ -34,44 +34,93 @@ function makePost() {
   blogPost.style.display = "none";
 }
 
-// function to attach picture to the blog post
-let myFiles = document.getElementById("file");
-let myImg = document.getElementById("myImg");
-function pickfile() {
-  let reader = new FileReader();
-  let fileData = myFiles.files[0];
-  console.log(myFiles.files);
-  reader.addEventListener("load", (e) => {
-    myImg.src = e.target.result;
-    console.log(e.target.result);
-  });
+// make the file input and delete image button hidden
+let fileInput = document.getElementById("file");
+fileInput.style.display = "none";
+let deleteImageBtn = document.getElementById("delete-image-btn");
+deleteImageBtn.style.display = "none";
 
-  if (fileData) {
-    let myUrl = reader.readAsDataURL(fileData);
-    console.log(myUrl);
+
+// function to attach picture to the blog post
+function toggleImageInput() {
+  let fileInput = document.getElementById("file");
+  let attachImageBtn = document.getElementById("attach-image-btn");
+  let previewImg = document.getElementById("previewImg");
+  let deleteImageBtn = document.getElementById("delete-image-btn");
+  if (fileInput.style.display === "none") {
+    fileInput.style.display = "block";
+    attachImageBtn.innerHTML = "Cancel";
+    previewImg.style.display = "none";
+    deleteImageBtn.style.display = "none";
+  } else {
+    fileInput.style.display = "none";
+    attachImageBtn.innerHTML = "Attach Image";
+    previewImg.style.display = "none";
+    deleteImageBtn.style.display = "none";
   }
 }
 
+// function to preview attached image
+function previewImage() {
+  let fileInput = document.getElementById("file");
+  let previewImg = document.getElementById("previewImg");
+  let deleteImageBtn = document.getElementById("delete-image-btn");
+  if (fileInput.files && fileInput.files[0]) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      previewImg.src = e.target.result;
+      previewImg.style.display = "block";
+      deleteImageBtn.style.display = "block";
+    };
+    reader.readAsDataURL(fileInput.files[0]);
+  }
+}
+
+// function to delete attached image
+function deleteImage() {
+  let fileInput = document.getElementById("file");
+  let previewImg = document.getElementById("previewImg");
+  let deleteImageBtn = document.getElementById("delete-image-btn");
+  fileInput.value = "";
+  previewImg.src = "";
+  previewImg.style.display = "none";
+  deleteImageBtn.style.display = "none";
+}
 
 // Function to create a new blog post and add it to the list of posts
 function sendPost() {
+  let fileInput = document.getElementById("file");
   let post = {
     title: postTitleInput.value,
     content: postContent.value,
     author: currentUser.username,
     time: new Date().toLocaleString(),
     likes: 0,
-    image: myImg.src
+    image: null
   };
-  blogPosts.push(post);
-  localStorage.setItem("blogPosts", JSON.stringify(blogPosts));
-  postTitleInput.value = "";
-  postContent.value = "";
-  myImg.src="";
-  viewFeed();
+  if (fileInput.files && fileInput.files[0]) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      post.image = e.target.result;
+      blogPosts.push(post);
+      localStorage.setItem("blogPosts", JSON.stringify(blogPosts));
+      postTitleInput.value = "";
+      postContent.value = "";
+      fileInput.value = "";
+      viewFeed();
+    };
+    reader.readAsDataURL(fileInput.files[0]);
+  } else {
+    blogPosts.push(post);
+    localStorage.setItem("blogPosts", JSON.stringify(blogPosts));
+    postTitleInput.value = "";
+    postContent.value = "";
+    fileInput.value = "";
+    previewImg.style.display = "none";
+    viewFeed();
+  }
 }
 
-// Function to display all blog posts
 // Function to display all blog posts
 function viewFeed() {
   let postsHTML = "";
@@ -83,7 +132,9 @@ function viewFeed() {
         post.likesBy && post.likesBy.includes(currentUser.username)
           ? "Unlike"
           : "Like";
-      let imageHTML = post.image ? `<img src="${post.image}" alt="Post image" class="post-image">` : "";
+      let imageHTML = post.image
+        ? `<img src="${post.image}" alt="Post image" class="post-image">`
+        : "";
       postsHTML += `
         <div id="postdata">
           <h1 class="post-title">${post.title}</h1>
@@ -115,10 +166,14 @@ function viewMyPosts() {
           post.likesBy && post.likesBy.includes(currentUser.username)
             ? "Unlike"
             : "Like";
+            let imageHTML = post.image
+        ? `<img src="${post.image}" alt="Post image" class="post-image">`
+        : "";
         postsHTML += `
           <div>
             <h1 class="post-title">${post.title}</h1>
             <p>${post.content}</p>
+            ${imageHTML}
             <p>Author: ${post.author}</p>
             <p>Time: ${post.time}</p>
             <p>Likes: <span id="likes-${i}">${post.likes}</span></p>
